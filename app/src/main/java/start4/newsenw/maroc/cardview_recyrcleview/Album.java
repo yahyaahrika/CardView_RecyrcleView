06799455979fa359a30ec85653c613aba41f77c2
yahya <zerowapo@gmail.com>
2016-08-12 23:21:14 +0100

    Initial commit

---
+++ app/src/main/java/start4/newsenw/maroc/cardview_recyrcleview/Album.java
@@ -0,0 +1,43 @@
+package start4.newsenw.maroc.cardview_recyrcleview;
+
+/**
+ * Created by HP on 12/08/2016.
+ */
+public class Album {
+    private String name;
+    private int numOfSongs;
+    private int thumbnail;
+
+    public Album() {
+    }
+
+    public Album(String name, int numOfSongs, int thumbnail) {
+        this.name = name;
+        this.numOfSongs = numOfSongs;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfSongs() {
+        return numOfSongs;
+    }
+
+    public void setNumOfSongs(int numOfSongs) {
+        this.numOfSongs = numOfSongs;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
